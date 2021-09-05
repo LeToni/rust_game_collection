a0922ff0d47f6f2d3fbf704feaa5bb2abbdcf306
Toni Le <toni.le@outlook.de>
2021-09-05 17:36:52 +0200

    Initial: add sprite to bottom

--- rust_invaders/src/main.rs
+++ rust_invaders/src/main.rs
@@ -12,5 +12,30 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    assert_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+    windows: Res<Windows>,
+) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // spawn sprite
+    let window = windows.get_primary().unwrap();
+    let bottom = -window.height() / 2.0;
+
+    commands.spawn_bundle(SpriteBundle {
+        material: materials.add(assert_server.load(PLAYER_SPRITE).into()),
+        transform: Transform {
+            translation: Vec3::new(0., bottom + 75.0 / 4.0 + 5., 10.0),
+            scale: Vec3::new(0.5, 0.5, 1.0),
+            ..Default::default()
+        },
+        ..Default::default()
+    });
 }
