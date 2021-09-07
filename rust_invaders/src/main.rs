72c69c15fd6cfbf8f4f258c90797390abe738f1d
Toni Le <toni.le@outlook.de>
2021-09-07 21:15:32 +0200

    Shooting Laser

--- rust_invaders/src/main.rs
+++ rust_invaders/src/main.rs
@@ -1,18 +1,21 @@
-use bevy::prelude::*;
+use bevy::{input::keyboard::keyboard_input_system, prelude::*};
 
 const PLAYER_SPRITE: &str = "player.png";
+const LASER_SPRITE: &str = "laser_a_01.png";
 const TIME_STEPS: f32 = 1.0 / 60.0;
 // region: Resources
 struct Materials {
     player_materials: Handle<ColorMaterial>,
+    laser_materials: Handle<ColorMaterial>,
 }
 // endregiom: Resources
 
 // region: Components
 struct Player;
-struct PlayerSpeed(f32);
+struct Laser;
+struct Speed(f32);
 // endregion: Components
-impl Default for PlayerSpeed {
+impl Default for Speed {
     fn default() -> Self {
         Self(500.0)
     }
@@ -34,6 +37,7 @@ fn main() {
             SystemStage::single(player_spawn.system()),
         )
         .add_system(player_movement.system())
+        .add_system(player_shoots.system())
         .run();
 }
 
@@ -48,6 +52,7 @@ fn setup(
     // create main resources
     commands.insert_resource(Materials {
         player_materials: materials.add(assert_server.load(PLAYER_SPRITE).into()),
+        laser_materials: materials.add(assert_server.load(LASER_SPRITE).into()),
     });
 }
 
@@ -67,12 +72,12 @@ fn player_spawn(mut commands: Commands, materials: Res<Materials>, windows: Res<
             ..Default::default()
         })
         .insert(Player)
-        .insert(PlayerSpeed::default());
+        .insert(Speed::default());
 }
 
 fn player_movement(
     keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&PlayerSpeed, &mut Transform, With<Player>)>,
+    mut query: Query<(&Speed, &mut Transform, With<Player>)>,
 ) {
     if let Ok((speed, mut transform, _)) = query.single_mut() {
         let dir = if keyboard_input.pressed(KeyCode::Left) {
@@ -84,5 +89,31 @@ fn player_movement(
         };
 
         transform.translation.x += dir * speed.0 * TIME_STEPS;
+    }
+}
+
+fn player_shoots(
+    mut commands: Commands,
+    materials: Res<Materials>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Transform, With<Player>)>,
+) {
+    if let Ok((player_tf, _)) = query.single_mut() {
+        if keyboard_input.pressed(KeyCode::Space) {
+            let pos_x = player_tf.translation.x;
+            let pos_y = player_tf.translation.y;
+
+            commands
+                .spawn_bundle(SpriteBundle {
+                    material: materials.laser_materials.clone(),
+                    transform: Transform {
+                        translation: Vec3::new(pos_x, pos_y, 0.0),
+                        ..Default::default()
+                    },
+                    ..Default::default()
+                })
+                .insert(Laser)
+                .insert(Speed::default());
+        }
     }
 }
