b7f4ee360940806589eaa697f8b332da221de0aa
Toni Le <toni.le@outlook.de>
2021-10-05 22:11:03 +0200

    Player spawn, movement, laser spawn

--- classic_space_invader/src/main.rs
+++ classic_space_invader/src/main.rs
@@ -2,13 +2,32 @@ use bevy::prelude::*;
 
 const WIN_H: f32 = 600.;
 const WIN_W: f32 = 600.;
+const SCALE: f32 = 0.5;
+const TIME_STEP: f32 = 1. / 60.;
+
+const PLAYER_SPRITE: &str = "player.png";
+const PLAYER_LASER_SPRITE: &str = "laser_player.png";
 
 // region: Resources
+
+struct Materials {
+    player: Handle<ColorMaterial>,
+    player_laser: Handle<ColorMaterial>,
+}
 
 // endregion: Resources
 
 // region Components
 
+struct Player;
+
+struct Speed(f32);
+
+impl Default for Speed {
+    fn default() -> Self {
+        Self(500.)
+    }
+}
 // endregion Components
 
 fn main() {
@@ -21,5 +40,82 @@ fn main() {
             ..Default::default()
         })
         .add_plugins(DefaultPlugins)
+        .add_startup_system(setup.system())
+        .add_startup_stage("game_setup", SystemStage::single(player_spawn.system()))
+        .add_system(player_movement.system())
+        .add_system(player_shoots.system())
         .run();
+}
+
+fn setup(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>,
+    mut materials: ResMut<Assets<ColorMaterial>>,
+) {
+    // camera
+    commands.spawn_bundle(OrthographicCameraBundle::new_2d());
+
+    // create main resources
+    commands.insert_resource(Materials {
+        player: materials.add(asset_server.load(PLAYER_SPRITE).into()),
+        player_laser: materials.add(asset_server.load(PLAYER_LASER_SPRITE).into()),
+    });
+}
+
+fn player_spawn(mut commands: Commands, materials: Res<Materials>) {
+    let bottom = -WIN_H / 2.;
+
+    commands
+        .spawn_bundle(SpriteBundle {
+            material: materials.player.clone(),
+            transform: Transform {
+                translation: Vec3::new(0., bottom + 75. / 4. + 5., 10.),
+                scale: Vec3::new(SCALE, SCALE, 1.),
+                ..Default::default()
+            },
+            ..Default::default()
+        })
+        .insert(Player)
+        .insert(Speed::default());
+}
+
+fn player_movement(
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Speed, &mut Transform, With<Player>)>,
+) {
+    if let Ok((speed, mut tf, _)) = query.single_mut() {
+        let dir = if keyboard_input.pressed(KeyCode::Left) {
+            -1.
+        } else if keyboard_input.pressed(KeyCode::Right) {
+            1.
+        } else {
+            0.
+        };
+
+        tf.translation.x += dir * speed.0 * TIME_STEP;
+    }
+}
+
+fn player_shoots(
+    mut commands: Commands,
+    materials: Res<Materials>,
+    keyboard_input: Res<Input<KeyCode>>,
+    mut query: Query<(&Transform, With<Player>)>,
+) {
+    if let Ok((tf, _)) = query.single_mut() {
+        if keyboard_input.pressed(KeyCode::Space) {
+            let pos_x = tf.translation.x;
+            let pos_y = tf.translation.y;
+
+            commands.spawn_bundle(SpriteBundle {
+                material: materials.player_laser.clone(),
+                transform: Transform {
+                    translation: Vec3::new(pos_x, pos_y, 0.),
+                    scale: Vec3::new(SCALE, SCALE, 1.),
+                    ..Default::default()
+                },
+                ..Default::default()
+            });
+        }
+    }
 }
